74d36e195068f560494b59a3bc8392c1937e10fa
Mariana Teut <mary_marianailea@yahoo.com>
2019-10-12 00:34:28 +0300

    solved CozalozaWoza

--- src/main/java/org/fastrackit/CozaLozaWoza.java
+++ src/main/java/org/fastrackit/CozaLozaWoza.java
@@ -1,9 +1,11 @@
 package org.fastrackit;
 
 public class CozaLozaWoza {
+    //1 make the program work with any blimits and end
+    // 2. extract the code to a separete method and invoke it inside main method
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 110 ; i++) {
+        for (int i = 1; i <= 110; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.print("CozaLoza ");
             } else if (i % 3 == 0) {
@@ -15,11 +17,29 @@ public class CozaLozaWoza {
             } else {
                 System.out.print(i + " ");
             }
-            if ( i % 11 == 0){
+            if (i % 11 == 0) {
                 System.out.println();
             }
         }
     }
 
-}
+    public void printCozaLozaWoza(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("CozaLoza ");
+            } else if (i % 3 == 0) {
+                System.out.print("Coza ");
+            } else if (i % 5 == 0) {
+                System.out.print("Loza ");
+            } else if (i % 7 == 0) {
+                System.out.print("Woza ");
+            } else {
+                System.out.print(i + " ");
+            }
+            if (i % 11 == 0) {
+                System.out.println();
+            }
 
+        }
+    }
+}
---
+++ src/main/java/org/fastrackit/Suma.java
@@ -0,0 +1,14 @@
+package org.fastrackit;
+
+public class Suma {
+
+    public static  void main(String[] args){
+        int x=10;
+        int y=5;
+        int sum= x + y;
+
+        System.out.println("suma este:" + sum);
+
+
+    }
+   }
